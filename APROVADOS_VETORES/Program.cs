9a50ba16df102f2f63053cc7d5ffb6872c043a08
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-08 16:57:37 -0300

    primeiro commit

---
+++ APROVADOS_VETORES/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace APROVADOS_VETORES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            string[] nome = new string[x];
+            double[] n1 = new double[x];
+            double[] n2 = new double[x];
+
+            for (int i = 0; i < x; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                nome[i] = s[0];
+                n1[i] = double.Parse(s[1], CultureInfo.InvariantCulture);
+                n2[i] = double.Parse(s[2], CultureInfo.InvariantCulture);
+            }
+
+            double media;
+            for (int i = 0; i < x; i++)
+            {
+                media = (n1[i] + n2[i]) / 2.0;
+                if (media >= 6)
+                {
+                    Console.WriteLine(nome[i]);
+                }
+            }
+
+        }
+    }
+}
+
